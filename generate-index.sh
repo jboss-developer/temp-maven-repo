d809614a2f6b024e389c137644f0407058c55256
Rafael Benevides <rafabene@gmail.com>
2013-10-30 11:11:49 -0200

    Update page title

--- generate-index.sh
+++ generate-index.sh
@@ -1,1 +1,1 @@
-tree -H . -o index.html -C -P "*.pom|*.jar|archetype-catalog.xml"
+tree -H . -T "JBoss Developer Temporary Maven Repository</br><i>for Quickstarts Contributors</i>" -o index.html -C -P "*.pom|*.jar|archetype-catalog.xml"
